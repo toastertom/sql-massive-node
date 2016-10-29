96d4fe52e0a2a884b9f5e8843f30466646ff80ee
ToasterTom <tate.thomas5595@gmail.com>
2016-10-29 13:43:42 -0600

    Created table with seral

---
+++ db/create_product.sql
@@ -0,0 +1,14 @@
+insert into products
+  (Name, Description, price, ImageURL)
+--values ('747', 200);
+
+
+-- Just for reference below.
+-- CREATE TABLE products 
+-- (
+--  ID serial PRIMARY KEY not null,
+--  Name varchar(255) NOT NULL,
+--  Description varchar(255),
+--  price int,
+--  ImageURL varchar(1000)
+-- );
